9338a530cd8263719ae4aeae9e4b02f6c3de7697
Armand LENGLET <armand@famille-lenglet.fr>
2024-01-24 08:35:11 +0100

    main

--- ORI/depVM9.1.X 1.sh
+++ ORI/depVM9.1.X 1.sh
@@ -136,46 +136,8 @@ fi
 
 
 ################
-#Main BDD
-if [[ "$VMTYPE" == "bdd" ]]
-then
-		echo -e "${W}##### BDD #####"
-		if [ $ISHYPERV = true ]
-		then
-			echo -e "${G}[+] CONFIGURE ETH1 NETWORK"
-			sed -i "s;#auto eth1;auto eth1;" /etc/network/interfaces
-			sed -i "s;#allow-hotplug eth1;allow-hotplug eth1;" /etc/network/interfaces
-			sed -i "s;#iface eth1 inet static;iface eth1 inet static;" /etc/network/interfaces
-			sed -i "s;.*address 10.42.42.X;\taddress $IPBDDINTERNE;" /etc/network/interfaces
-			sed -i "s;.*netmask 255.255.255.0;\tnetmask 255.255.255.0;" /etc/network/interfaces
-			systemctl restart networking
-		fi
-		
-        #Change IPTABLES
-		#IF ESX, USE $IPAPP,ELSE, USE $IPINTERNE
-		echo -e "${G}[+] CONFIGURE IPTABLES"
-		if [[ $ISHYPERV == false ]]; then
-			sed -i "s;-A INPUT -p tcp -m tcp --dport 3306 -s 10.42.42.2 -j ACCEPT;-A INPUT -p tcp -m tcp --dport 3306 -s 10.42.42.101 -j ACCEPT;" /etc/iptables.up.rules
-		fi
-		
-		if [[ $IPAPPBIS != "" ]]
-        then
-			sed -i "/.*CONNEXION TO MARIADB.*/a -A INPUT -p tcp -m tcp --dport 3306 -s 10.42.42.105 -j ACCEPT" /etc/iptables.up.rules
-		fi
-		if [[ $IPINTEROP != "" ]]
-        then
-			sed -i "/.*CONNEXION TO MARIADB.*/a -A INPUT -p tcp -m tcp --dport 3306 -s 10.42.42.103 -j ACCEPT" /etc/iptables.up.rules
-        fi
-		iptables-restore < /etc/iptables.up.rules
-
-        #Crontab
-		echo -e "${G}[+] EDIT CRONTAB"
-        crontab -l | sed '/mysql/s/^#//g' | crontab -
-		
-################
 #Main APP
-elif [[ "$VMTYPE" == "app" ]]
-then
+
 		echo -e "${W}##### APP #####"
 
         if [ $ISHYPERV = true ]
@@ -209,26 +171,16 @@ then
 			xfs_growfs /mnt/data1/
         fi
 		
-        #IPP
-		if [[ $IPP != "" ]]
-		then
-			echo -e "${G}[+] SET IPP"
-			echo $IPP > /opt/etmi/ECS/.shistukendayo
-		fi
+
 		
-		#config site ssl apache2
-		if [[ $NDD != "" ]]
-		then
-			echo -e "${G}[+] Set $NDD in vhost 1-ssl.conf"
-			sed -i "s;NDD;$NDD;" /etc/apache2/sites-available/1-ssl.conf
-		fi
+
 		
         #Lien ECSIMAGING vers BDD
 		echo -e "${G}[+] CONFIGURE CONFIG.INI"
 		sed -i 's;.*sql_password.*;sql_password=daPD*taba-hzpsqhNA:a;' /opt/etmi/ECS/config.ini
 		if [[ $ISHYPERV == "false" ]]
 		then
-			sed -i "s;.*sql_hostname.*;sql_hostname=$IPBDD;" /opt/etmi/ECS/config.ini
+			sed -i "s;.*sql_hostname.*;sql_hostname=10.42.42.102;" /opt/etmi/ECS/config.ini
 		fi
 		
         #UUID IMGPRD-APP
@@ -244,49 +196,6 @@ then
 			ERRORS+=("BDDUID")
 		fi
 		
-        #Activation des services PACS
-		echo -e "${G}[+] ENABLE LICENCES PACS${W}"
-        #/opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xt3rdParty | sudo xargs /opt/etmi/ECS/xtLicence -register xt3rdParty
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xtDiskMgr | sudo xargs /opt/etmi/ECS/xtLicence -register xtDiskMgr
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xtImporter | sudo xargs /opt/etmi/ECS/xtLicence -register xtImporter
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xtMonitor | sudo xargs /opt/etmi/ECS/xtLicence -register xtMonitor
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xtQRSCP | sudo xargs /opt/etmi/ECS/xtLicence -register xtQRSCP
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xtQRSCU | sudo xargs /opt/etmi/ECS/xtLicence -register xtQRSCU
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xtRecorder | sudo xargs /opt/etmi/ECS/xtLicence -register xtRecorder
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xtStoreSCP | sudo xargs /opt/etmi/ECS/xtLicence -register xtStoreSCP
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xtStoreSCU | sudo xargs /opt/etmi/ECS/xtLicence -register xtStoreSCU
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xtUploader | sudo xargs /opt/etmi/ECS/xtLicence -register xtUploader
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xtWLSCP | sudo xargs /opt/etmi/ECS/xtLicence -register xtWLSCP
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen ecswss4web | sudo xargs /opt/etmi/ECS/xtLicence -register ecswss4web
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xtNotifier | sudo xargs /opt/etmi/ECS/xtLicence -register xtNotifier
-        /opt/etmi/ECS/xtLicence -systemid | xargs /opt/etmi/ECS/xtLicence -keygen xtPreFetcher | sudo xargs /opt/etmi/ECS/xtLicence -register xtPreFetcher
-        /opt/etmi/ECS/xtInit start
-		
-        #Configuration du client SMTP
-		if [[ "$HOSTMAIL" != "" || "$USERMAIL" != "" || "$PASSWORDMAIL" != "" ]]
-		then
-			echo -e "${G}[+] SET MAIL MSMTPRC${W}"
-			sed -i "s;host mail.xxx;host $HOSTMAIL;" /etc/msmtprc
-			sed -i "s;user noreply@xxx;user $USERMAIL;" /etc/msmtprc
-			sed -i "s;password XXXXXXXXX;password $PASSWORDMAIL;" /etc/msmtprc
-			sed -i "s;from noreply@xxx;from $USERMAIL;" /etc/msmtprc
-			chmod 0644 /etc/msmtprc
-			chown www-data:www-data /etc/msmtprc
-			if echo -e "Subject: Test Mail\r\n\r\nThis is a test mail" | msmtp --debug -t "itimaging@evolucare.com" 2>/dev/null; then
-				echo -e "${G}[+] SEND TEST MAIL MSMTPRC: OK"
-			else
-				echo -e "${R}[-] SEND TEST MAIL MSMTPRC: ERROR, PLEASE CHECK /ETC/MSMTPRC CONFIG"
-				ERRORS+=("MSMTP")
-			fi
-			
-			for PHPINIFILE in `find /etc/php*/ -name "php.ini"`; do sed -i 's/-C\ \/etc\/msmtprc_php//g' $PHPINIFILE; done
-			for PHPINIFILE in `find /etc/php*/ -name "php.ini"`; do sed -i 's/-C\ \/etc\/msmtprc//g' $PHPINIFILE; done
-			for PHPINIFILE in `find /etc/php*/ -name "php.ini"`; do sed -i "s/--logfile\ \/var\/log\/msmtp.log//g" $PHPINIFILE; done
-			rm -f /var/log/msmtp.log
-			rm -f /etc/msmtprc_php
-			grep --silent "syslog on" /etc/msmtprc || echo "syslog on" >> /etc/msmtprc
-		fi
-	
 		#Config fstab
 		if [[ $IPNAS != "" ]]
 		then
@@ -304,126 +213,6 @@ then
 		#(crontab -l 2>/dev/null; echo -e "\n# Zip SQL\n#0 23   * * * /opt/scripts/sql-dumpzip.sh --rmsqlsrc --nodate /mnt/backupnas >> /var/log/etmi/scripts.log 2>&1") | crontab -
 		
         
-################
-#Main HUB
-elif [[ "$VMTYPE" == "hub" ]]
-then
-		echo -e "${W}##### HUB #####"
-		
-		echo -e "${G}[+] CONFIG HUB SSH"
-		if [ $ISTX = false ]
-        then
-		#Pas TX
-			if [[ $IPAPP != "" ]]
-			then
-				sed -i 's;.*IMGPRD-APP|support|.*;"IMGPRD-APP|support|'$IPAPP'|22000";' /home/support/.imgprd-hub-sshportal.sh
-			fi
-			if [[ $IPBDD != "" ]]
-			then
-				sed -i 's;.*IMGPRD-BDD|support|.*;"IMGPRD-BDD|support|'$IPBDD'|22000";' /home/support/.imgprd-hub-sshportal.sh
-			fi
-		else
-		#TX
-			if [[ $IPAPP != "" ]]
-			then
-				sed -i 's;.*IMGPRD-APP|support|.*;"IMGPRD-APP-TX|support|'$IPAPP'|22000";' /home/support/.imgprd-hub-sshportal.sh
-			fi
-			if [[ $IPBDD != "" ]]
-			then
-				sed -i 's;.*IMGPRD-BDD|support|.*;"IMGPRD-BDD-TX|support|'$IPBDD'|22000";' /home/support/.imgprd-hub-sshportal.sh
-			fi
-		fi
-		
-		if [[ $IPINTEROP != "" ]]
-		then
-			sed -i 's;.*IMGPRD-INTEROP|support|.*;"IMGPRD-INTEROP|support|'$IPINTEROP'|22000";' /home/support/.imgprd-hub-sshportal.sh
-		fi
-		
-		if [[ $IPUVIEW != "" ]]
-		then
-			sed -i 's;.*IMGPRD-UVIEW|support|.*;"IMGPRD-UVIEW|support|'$IPUVIEW'|22000";' /home/support/.imgprd-hub-sshportal.sh
-		fi
-		sed -i "/.*IMGTX-APP-TX-XXX|support|.*/d" /home/support/.imgprd-hub-sshportal.sh
-		sed -i "/.*IMGTX-BDD-TX-XXX|support|.*/d" /home/support/.imgprd-hub-sshportal.sh
-		
-        #Configuration serveur apache
-		echo -e "${G}[+] CONFIG APACHE CONF"
-		if [[ $NDD != "" ]]
-		then	
-			sed -i "s;Define websiteurl ecstest.evolucare.com;Define websiteurl $NDD;" /etc/apache2/sites-enabled/imgprd-hub1.conf	
-		fi
-		if [[ $IPAPP != "" ]]
-		then
-			sed -i "s;Define websiteip 192.168.10.54;Define websiteip 10.42.42.101;" /etc/apache2/sites-enabled/imgprd-hub1.conf
-		fi
-		
-		echo -e "${G}[+] CONFIG NGINX CONF"
-        #Configuration NGINX
-		if [[ $IPAPP != "" ]]
-		then
-			sed -i "s|server 192.168.10.54:4545 max_fails=2;|\tserver 10.42.42.101:4545 max_fails=2;|" /etc/nginx/sites-enabled/imgprd-hub1
-        fi
-		if [[ $NDD != "" ]]
-		then
-			sed -i "s|server_name ecstest.evolucare.com;|\tserver_name $NDD;|" /etc/nginx/sites-enabled/imgprd-hub1
-		fi
-		
-		systemctl restart apache2
-		systemctl restart nginx
-		
-        #Generer cle ssh avec support
-		echo -e "${G}[+] GENERATE SSH KEY"
-        su - support -c 'ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsa -q -N ""'
-        chmod 600 /home/support/.ssh/id_rsa; chmod 600 /home/support/.ssh/id_rsa.pub
-
-
-
-		#IPTABLES
-		IPLOCALCLIENT=`ip route | grep "eth*" | grep -wv -e default | cut -d ' ' -f1`
-		LISTEIP=("$IPLOCALCLIENT" "212.129.3.250" "212.129.3.252" "37.58.224.107" "37.58.224.208" "176.162.27.198" "37.58.224.156" "37.58.170.170" "213.151.174.242" "37.58.254.83" "37.58.224.14" "86.198.142.164" "90.119.64.72")
-		COMMENT='# SERVEUR FILTRAGE BY AGENCE ;)'
-		IPTABLES="/etc/iptables.up.rules"
-		IPTABLESBKP="/etc/iptables.up.rules.bkp"
-		LISTEPORTS+=("22000")
-
-		#BACKUP
-		echo -e "${G}[+] IPTABLES BACKUP : $IPTABLESBKP"
-		cp $IPTABLES $IPTABLESBKP
-		
-		for PORT in ${LISTEPORTS[@]}; do
-			#ADD COMMENT
-			if  ! grep -q "$COMMENT" $IPTABLES
-			then
-				printf '%s\n' 'set ignorecase' '$+?COMMIT?i' '# SERVEUR FILTRAGE BY AGENCE ;)' . x | ex $IPTABLES
-			fi
-
-			#FOR EACH IP
-			for IP in ${LISTEIP[@]}
-			do
-                if  ! grep -q "\-A INPUT -p tcp -m tcp --dport $PORT -s $IP -j ACCEPT" $IPTABLES
-                then
-					printf '%s\n' 'set ignorecase' '$+?COMMIT?i' "-A INPUT -p tcp -m tcp --dport $PORT -s $IP -j ACCEPT" . x | ex $IPTABLES
-                    echo "{G}[+] ADD RULE: allow $IP to port $PORT"
-                fi
-			done
-
-			#COMMENT ALL IMPACTED
-			sed -e "/-A INPUT -p tcp --dport $PORT -m comment --comment \"APACHE - NO LIMIT HTTPS CONNEXION TCP\" -j ACCEPT/ s/^#*/#/" -i $IPTABLES
-
-			# IF PORT SSH IN LISTEPORTS
-			if [[ $PORT == "22000" ]]; then
-				sed -e "/-A INPUT -p tcp --dport 22000 -m comment --comment \"SSHD - NO LIMIT SSH CONNEXION TCP\" -j ACCEPT/ s/^#*/#/" -i $IPTABLES
-			fi
-			echo "${G}[+] COMMENT: disable all IP from $PORT"
-		done
-
-		iptables-restore < $IPTABLES
-		
-		#PATCH CRISE
-		if [[ -f /opt/scripts/scripts.conf ]]; then sed -i "/^SRV_RELEASE_HTACCESS=/{h;s/=.*/='prods-autoupdate|54\&7y5fQweSt'/};" /opt/scripts/scripts.conf; fi
-		if [[ -f /opt/scripts/scripts.conf ]]; then sed -i "/^SRV_MAJ_HTACCESS=/{h;s/=.*/='prods-ecsupgrader|Z@\$TeEDS84p7'/};" /opt/scripts/scripts.conf; fi
-		if [[ -f /var/www2/.htpasswd ]]; then echo -e 'support:$2y$05$BGbL.F0DFTTcw.xwSW3pKuv6.0Xogy4lNm7e17cuQODD1e49GAeNW' > /var/www2/.htpasswd; fi
-
 
 ################
 #Main INTEROP
